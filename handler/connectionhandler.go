49dbb4196909272ad8e02f8583cdb0c8b625d5bd
Carlosokumu <69672450+Carlosokumu@users.noreply.github.com>
2022-01-06 13:38:38 +0300

    client build up

--- handler/connectionhandler.go
+++ handler/connectionhandler.go
@@ -1,7 +1,30 @@
 package handler
 
-import "fmt"
+import (
+	"encoding/gob"
+	"log"
+	"net"
 
-func GetData() {
-	fmt.Println("Imported")
+	"github.com/gorilla/mux"
+)
+
+/*
+
+   Once the clients dials into the connection with the right port,the http server starts serving content
+   based on the matched url.[using mux router]
+
+*/
+func Client(port string) {
+
+	c, err := net.Dial("tcp", port)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//Inialize an instance of the Mux Router and encode it as A pointer
+	r := mux.NewRouter()
+	gob.NewEncoder(c).Encode(*r)
+	c.Close()
+
 }
